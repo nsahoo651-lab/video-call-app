cc9201061a13367aca93389925fff0bb2fb19b3b
nsahoo651-lab <nsahoo651@gmail.com>
2026-04-26 14:42:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'naresh_pro_meet_2026'
-# 6 logon ka load aur badi photos share karne ke liye settings
-socketio = SocketIO(app, cors_allowed_origins="*", max_decode_packets=5000000)
+app.config['SECRET_KEY'] = 'naresh_pro_audio_video'
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 
 @app.route('/')
 def index():
@@ -15,21 +14,13 @@ def index():
 def on_join(data):
     room = 'naresh_room'
     join_room(room)
-    # Dusre logon ko naye bande ki jankari bhejna
     emit('user-joined', data, to=room, include_self=False)
-
-@socketio.on('chat_message')
-def handle_message(data):
-    emit('chat_message', data, to='naresh_room', include_self=False)
-
-@socketio.on('media_share')
-def handle_media(data):
-    emit('receive_media', data, to='naresh_room', include_self=False)
 
 @socketio.on('signal')
 def handle_signal(data):
+    # Ye line video aur audio ka data ek phone se dusre phone ko bhejti hai
     emit('signal', data, to='naresh_room', include_self=False)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=port)
