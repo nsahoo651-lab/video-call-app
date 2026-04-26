2d9f5d1d05157d33dcde0c5d2e786419ad5fbfe6
nsahoo651-lab <nsahoo651@gmail.com>
2026-04-26 12:45:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-# Secret key security ke liye zaroori hai
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="*")
+app.config['SECRET_KEY'] = 'naresh_secret_123'
+# CORS allow-all taaki koi bhi jud sake
+socketio = SocketIO(app, cors_allowed_origins="*", max_decode_packets=1000000)
 
 @app.route('/')
 def index():
@@ -13,10 +13,14 @@ def index():
 
 @socketio.on('signal')
 def handle_signal(data):
-    # Ek phone se dusre phone tak signal bhejta hai
+    # Multi-user signaling: Jo bhi data aaye use baaki sabko bhej do
     emit('signal', data, broadcast=True, include_self=False)
 
+@socketio.on('chat_message')
+def handle_message(data):
+    # Chat message aur file sharing data ko sabko bhejna
+    emit('chat_message', data, broadcast=True, include_self=False)
+
 if __name__ == '__main__':
-    # Render khud port decide karta hai, isliye hum os.environ use kar rahe hain
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
