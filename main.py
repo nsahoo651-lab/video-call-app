3c30ab599eac6b02ef636c7e04b8b3125c2265fa
nsahoo651-lab <nsahoo651@gmail.com>
2026-04-26 14:23:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'naresh_pro_master_2026'
-# Badi files/photos ke liye memory badha di hai
+app.config['SECRET_KEY'] = 'naresh_pro_meet_2026'
+# 6 logon ka load aur badi photos share karne ke liye settings
 socketio = SocketIO(app, cors_allowed_origins="*", max_decode_packets=5000000)
 
 @app.route('/')
@@ -15,16 +15,15 @@ def index():
 def on_join(data):
     room = 'naresh_room'
     join_room(room)
+    # Dusre logon ko naye bande ki jankari bhejna
     emit('user-joined', data, to=room, include_self=False)
 
 @socketio.on('chat_message')
 def handle_message(data):
-    # Chat message sabko bhej do
     emit('chat_message', data, to='naresh_room', include_self=False)
 
 @socketio.on('media_share')
 def handle_media(data):
-    # Photo/File sabko bhej do
     emit('receive_media', data, to='naresh_room', include_self=False)
 
 @socketio.on('signal')
