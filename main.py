b9c7df7004a7b1a038009a0ac7be752a933517d5
nsahoo651-lab <nsahoo651@gmail.com>
2026-04-26 13:49:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import os
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'naresh_pro_meet_2026'
-socketio = SocketIO(app, cors_allowed_origins="*")
+app.config['SECRET_KEY'] = 'naresh_pro_master_2026'
+# Badi files/photos ke liye memory badha di hai
+socketio = SocketIO(app, cors_allowed_origins="*", max_decode_packets=5000000)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@socketio.on('join')
+def on_join(data):
+    room = 'naresh_room'
+    join_room(room)
+    emit('user-joined', data, to=room, include_self=False)
+
+@socketio.on('chat_message')
+def handle_message(data):
+    # Chat message sabko bhej do
+    emit('chat_message', data, to='naresh_room', include_self=False)
+
+@socketio.on('media_share')
+def handle_media(data):
+    # Photo/File sabko bhej do
+    emit('receive_media', data, to='naresh_room', include_self=False)
+
 @socketio.on('signal')
 def handle_signal(data):
-    emit('signal', data, broadcast=True, include_self=False)
+    emit('signal', data, to='naresh_room', include_self=False)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
