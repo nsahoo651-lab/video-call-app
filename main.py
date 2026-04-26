627d7f3fb3434acc966034258049a6eb7eda4b72
nsahoo651-lab <nsahoo651@gmail.com>
2026-04-26 13:13:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'naresh_secret_123'
-socketio = SocketIO(app, cors_allowed_origins="*", max_decode_packets=1000000)
+app.config['SECRET_KEY'] = 'naresh_pro_key_99'
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def index():
@@ -12,13 +12,14 @@ def index():
 
 @socketio.on('signal')
 def handle_signal(data):
+    # Signaling for video call
     emit('signal', data, broadcast=True, include_self=False)
 
 @socketio.on('chat_message')
 def handle_message(data):
+    # Sending chat messages to everyone
     emit('chat_message', data, broadcast=True, include_self=False)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
-    
